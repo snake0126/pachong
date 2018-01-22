1e72e7631d420f1e65a2fb5eb9d49826d37f4b05
snake0126 <fanfu0126@gmail.com>
2018-01-22 18:45:16 +0800

    Update yahooweatherapi_pachong.py

--- yahooweatherapi_pachong.py
+++ yahooweatherapi_pachong.py
@@ -12,7 +12,24 @@ realurl = url+City+values
 urlread = urllib2.urlopen(realurl)
 resp = urlread.read()
 datadict = json.loads(resp)
-#pprint(datadict)
+pprint(datadict)
 
-print"%s:%s" % ("today high temp", float(datadict[u'query'][u'results'][u'channel'][u'item'][u'forecast'][0]["high"]-32)/1.8)
-print"%s:%s" % ("today low temp", datadict[u'query'][u'results'][u'channel'][u'item'][u'forecast'][0][u'low'])
+hightemp = datadict[u'query'][u'results'][u'channel'][u'item'][u'forecast'][0]["high"]
+hightemp = str(hightemp)
+hightemp = int(hightemp)
+hightempC = ((hightemp)-32.0)//1.8+1
+
+lowtemp = datadict[u'query'][u'results'][u'channel'][u'item'][u'forecast'][0][u'low']
+lowtemp = str(lowtemp)
+lowtemp = int(lowtemp)
+lowtempC = ((lowtemp)-32.0)//1.8+1
+
+todayweather = datadict[u'query'][u'results'][u'channel'][u'item'][u'forecast'][0][u'text']
+weatherlist = { 'Showers': u'阵雨', 'Snow': u'雪' ,'Rain': u'雨' , 'Rain And Snow':'雨夹雪', 'Partly Cloudy':'局部阴天'}
+
+print"%s:%s" % (u"今日天气", weatherlist[todayweather])
+print"%s:%s%s" % (u"今日最高温度", hightempC, u'°C')
+print"%s:%s%s" % (u"今日最低温度", lowtempC,u'°C')
+
+if __name__ == '__mian__':
+    main()
